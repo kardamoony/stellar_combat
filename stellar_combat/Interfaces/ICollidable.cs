b2e81e65ed2d1195d9a5a53db90a5ec2056c27b9
Daria Karamova <d.karamova@crazypanda.ru>
2023-03-10 15:44:51 +0300

    collision regions system

---
+++ stellar_combat/Interfaces/ICollidable.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+using IoC.Interfaces;
+
+namespace StellarCombat.Interfaces
+{
+    public interface ICollidable
+    {
+        Vector2 Position { get; set; }
+        ICommand? CheckCollisionCmd { get; set; }
+    }
+}
+
