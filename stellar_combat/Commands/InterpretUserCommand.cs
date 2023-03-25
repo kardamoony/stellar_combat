b184ad4c0a9b73d5b77c8e7197b03f2be3c94283
Daria Karamova <d.karamova@crazypanda.ru>
2023-03-25 11:02:04 +0300

    interpret command

---
+++ stellar_combat/Commands/InterpretUserCommand.cs
@@ -0,0 +1,26 @@
+﻿using IoC;
+using IoC.Interfaces;
+using StellarCombat.Interfaces;
+
+namespace StellarCombat.Commands;
+
+public class InterpretUserCommand : ICommand
+{
+    private readonly IUserContext _context;
+
+    public InterpretUserCommand(IUserContext context)
+    {
+        _context = context;
+    }
+
+    public void Execute()
+    {
+        var objectKey = _context.ObjectId + _context.UserId;
+        var actionObject = Container.Resolve<object>(objectKey, _context.ObjectArgs);
+        
+        var commandArgs = new List<object> { actionObject };
+        commandArgs.AddRange(_context.CommandArgs);
+
+        Container.Resolve<ICommand>(_context.CommandId, commandArgs.ToArray()).Execute();
+    }
+}
